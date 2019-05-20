d60d7ffc9b1a53502b52227086bbe8f0f22b8502
nathalialgomes <50590912+nathalialgomes@users.noreply.github.com>
2019-05-20 13:46:31 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,59 @@
+from random import randint
+
+print("Jogo de Dados;")                                                                                                                 
+
+dados = randint(1, 6)
+print("Dados 1:", dados1)
+
+dados = randint(1, 6)
+print("Dados 2:", dados2)
+
+dados = randint(1, 6)
+print("Dados 3:", dados3)
+
+dados = randint(1, 6)
+print("Dados 4:", dados4)
+
+print("jogador 1:") , jogador 1)
+print("jogador 2:") , jogador 2
+
+if Jogador1 > jogador2
+print(jogador 2 venceu)
+else:
+    ifJogador2 > jogador1
+print(jogador 1 venceu)
+else:
+    print("empate")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
